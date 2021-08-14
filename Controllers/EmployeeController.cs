66d8e3bad55b7a146ddad72a1ff7f8d511a56ef6
Abdul Moiz <moiza0799@gmail.com>
2021-08-14 10:48:12 -0700

    Initial Forms Added

--- Controllers/EmployeeController.cs
+++ Controllers/EmployeeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EmployeeManagementSystem.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -12,6 +13,31 @@ namespace EmployeeManagementSystem.Controllers
         public ActionResult New()
         {
             return View();
+        }
+        public ActionResult AddEmployee(Employee employee)
+        {
+            var db = new dbContext();
+            
+            // get existing department and designation with with a particular ID
+            var department = db.Departments.FirstOrDefault(x => x.Id == employee.Department.Id);
+            employee.Department = department;
+
+            var designation = db.Designations.FirstOrDefault(x => x.Id == employee.Designation.Id);
+            employee.Designation = designation;
+
+            db.Employees.Add(employee);
+            db.SaveChanges();
+
+            return View("New");
+        }
+        public ActionResult AllEmployee()
+        {
+            var model = new List<Employee>();
+
+            var db = new dbContext();
+            model = db.Employees.ToList();
+
+            return View(model);
         }
     }
 }
---
+++ Main.cs
@@ -0,0 +1,28 @@
+﻿using EmployeeManagementSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EmployeeManagementSystem
+{
+    public class Main
+    {
+        public static List<Department> GetDepartment()
+        {
+            var model = new List<Department>();
+
+            var db = new dbContext();
+            model = db.Departments.ToList();
+            return model;
+        }
+        public static List<Designation> GetDesignamtion()
+        {
+            var model = new List<Designation>();
+
+            var db = new dbContext();
+            model = db.Designations.ToList();
+            return model;
+        }
+    }
+}
